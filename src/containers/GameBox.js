3fb54fc9e93a50bfdc4b192996f8bce21d2ce57f
Mike-Heneghan <m.heneghan@live.co.uk>
2018-06-13 15:19:09 +0100

    Stopped user from being able to double click box.

--- src/containers/GameBox.js
+++ src/containers/GameBox.js
@@ -5,39 +5,44 @@ import GameGrid from '../components/GameGrid';
 class GameBox extends React.Component {
   constructor(props){
     super(props);
-      this.state = {
-        grid: [[" ","X "," "],[" "," "," "],[" "," "," "]]
-      }
-
-      this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.state = {
+      grid: [[" "," "," "],[" "," "," "],[" "," "," "]],
+      turnNumber: 1
     }
-
-    handleButtonClick(event){
-      const coordinates = event.target.value
-      const yCoord = coordinates[0]
-      const xCoord = coordinates[2]
-      console.log("x", xCoord);
-      console.log("y", yCoord);
-      const newGrid = this.state.grid;
-      console.table(newGrid);
-
-      newGrid[yCoord][xCoord] = "X";
-
-      console.table(newGrid);
-      this.setState({grid: newGrid})
-
-    }
-
-  render() {
-
-    return (
-      <div>
-      <Header title='Play Tic Tac Toe'/>
-      <GameGrid gameGrid={this.state.grid} handleButtonClick={this.handleButtonClick}/>
-    </div>
-
-    )
+    this.handleButtonClick = this.handleButtonClick.bind(this)
   }
-}
 
-export default GameBox;
+  handleButtonClick(event){
+    const coordinates = event.target.value
+    const yCoord = coordinates[0]
+    const xCoord = coordinates[2]
+    const newGrid = this.state.grid
+
+    if ( newGrid[yCoord][xCoord] != " "){
+      return
+    }
+
+    if(this.state.turnNumber %2 !== 0){
+      newGrid[yCoord][xCoord] = "X"}
+    else {
+        newGrid[yCoord][xCoord] = "0"
+      }
+    this.setState({grid: newGrid})
+    this.setState(prevState => {turnNumber: (prevState.turnNumber ++)})
+    }
+
+
+
+    render() {
+
+      return (
+        <div>
+          <Header title='Play Tic Tac Toe'/>
+          <GameGrid gameGrid={this.state.grid} handleButtonClick={this.handleButtonClick}/>
+        </div>
+
+      )
+    }
+  }
+
+  export default GameBox;
